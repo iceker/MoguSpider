06fd654fa5b47221a417a7e1321f0122d380518e
iceker <v-ralv@hotmail.com>
2018-03-28 21:35:36 +0800

    update

--- MoguSpider/spiders/wsy.py
+++ MoguSpider/spiders/wsy.py
@@ -16,18 +16,21 @@ class WsySpider(scrapy.Spider):
         for link in firstlinks:
             url = link.css("a::attr(href)").extract_first("")
             match_re = re.match(".*?(\d+).*", url)
-            pid = match_re.group(1)
-            firstId = pid
-            firstName = link.css("a::text").extract_first("")
-            wsyItem = WsySPiderItem()
-            wsyItem["Name"] = firstName
-            wsyItem["SourceId"] = firstId
-            wsyItem["ParentId"] = "0"
-            wsyItem['CreatedDate'] = datetime.datetime.now()
-            wsyItem['SyncTime'] = datetime.datetime.now()
-            url = parse.urljoin(response.url, url)
-            request = scrapy.Request(parse.urljoin(response.url, url), callback=self.parse2)
-            request.meta["parentId"] = firstId
+            if match_re is not None:
+                pid = match_re.group(1)
+                firstId = pid
+                firstName = link.css("a::text").extract_first("")
+                wsyItem = WsySPiderItem()
+                wsyItem["Name"] = firstName
+                wsyItem["SourceId"] = firstId
+                wsyItem["ParentId"] = "0"
+                wsyItem['CreatedDate'] = datetime.datetime.now()
+                wsyItem['SyncTime'] = datetime.datetime.now()
+                yield wsyItem
+                url = parse.urljoin(response.url, url)
+                request = scrapy.Request(parse.urljoin(response.url, url), callback=self.parse2)
+                request.meta['parentId'] = firstId
+                yield request
         pass
     def parse2(self,response):
         secondlinks = response.css(".type_section:nth-child(2) li a")
@@ -37,5 +40,12 @@ class WsySpider(scrapy.Spider):
             pid = match_re.group(1)
             secondid = pid
             secondName = link.css("a::text").extract_first("")
-            i
+            if secondid!='0':
+                wsyItem = WsySPiderItem()
+                wsyItem["Name"] = secondName
+                wsyItem["SourceId"] = secondid
+                wsyItem["ParentId"] = response.meta['parentId']
+                wsyItem['CreatedDate'] = datetime.datetime.now()
+                wsyItem['SyncTime'] = datetime.datetime.now()
+                yield wsyItem
         pass
